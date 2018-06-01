96c99cbddf90feaa4269a64aff19f1110e14c2f9
Farah Nedjadi <farah.nedjadi@epita.fr>
2018-06-01 12:17:16 +0200

    [FRONT] Add content to FAQ and Contact pages

--- frontend/src/contact/contact.js
+++ frontend/src/contact/contact.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
+// https://www.spotify.com/us/about-us/contact/
+
 class Contact extends React.Component {
   render() {
     return (
       <div id='contact'>
-        Contact 
+        <h1>About us</h1> 
+        <p>
+          With Moodify, it’s easy to find the right music for your right mood.
+          There are millions of tracks on Spotify. So whether you’re excited, dreamy or cranky, the right music is always at your fingertips.
+          Choose your moods and we will make you a playlist to listen to on your Spotify account.
+          <br/>
+          Soundtrack your life with Moodify.
+        </p>
+
+        <h1>Customer Service and Support</h1> 
+        <a href="/faq"> Help site. </a>
+
+        <h1>Moodify HQ</h1>
+        <address>
+        <strong> Moodify-EPITA FR Inc </strong>
+        <br/>
+        14-16 Rue Voltaire
+        <br/>
+        6th Floor
+        <br/>
+        Le Kremlin-Bicêtre, 94270
+        <br/>
+        France
+        </address>
       </div>
     );
   }
--- frontend/src/faq/faq.js
+++ frontend/src/faq/faq.js
@@ -4,7 +4,12 @@ class FAQ extends React.Component {
   render() {
     return (
       <div id='faq'>
-        FAQ 
+        <h1>Ask the Community</h1>
+        <h2>Have questions? Our worldwide Community of expert fans can answer them. Have answers? Join the Community and help!</h2>
+        <ul>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com"> Facebook </a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://community.spotify.com"> Spotify </a></li>
+        </ul>
       </div>
     );
   }
