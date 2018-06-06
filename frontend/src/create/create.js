41b86b3bff891f8dc3d22123ebc8ac66630a423f
Farah Nedjadi <farah.nedjadi@epita.fr>
2018-06-06 18:40:34 +0200

    [FRONT] Table on the create page w/ delete row option

--- frontend/src/create/create.js
+++ frontend/src/create/create.js
@@ -1,13 +1,89 @@
 import React from 'react';
+import { Table } from 'element-react';
+import { Button } from 'element-react';
+
+import 'element-theme-default';
 
 class Create extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      columns: [
+        {
+          type: 'index'
+        },
+        {
+          label: "Artiste",
+          prop: "artiste",
+          sortable: true,
+          width: 150,
+          render: function(data){
+            return (
+            <span>
+              <span style={{marginLeft: '10px'}}>{data.artiste}</span>
+            </span>)
+          }
+        },
+        {
+          label: "Song",
+          prop: "title",
+          width: 160
+        },
+        {
+          label: "Album",
+          prop: "album",
+          width: 160
+        },
+        {
+          label: "",
+          prop: "",
+          width: 110,
+          render: (row, column, index) => {
+            return (
+              <span>
+                <Button type="danger" size="small" icon="delete" onClick={this.deleteRow.bind(this, index)}>Delete</Button>
+              </span>
+            )
+          }
+        }
+      ],
+      data: [{
+        artiste: 'Lady Gaga',
+        title: 'Bad Romance',
+        album: 'Bad Romance',
+       }]
+    }
+  }
+
+  deleteRow(index) {
+    const { data } = this.state;
+    data.splice(index, 1);
+    this.setState({
+      data: [...data]
+    })
+  }
+  
   render() {
+    return (
+      <Table
+        className='create-table'
+        style={{width: '100%', height: '100%'}}
+        columns={this.state.columns}
+        data={this.state.data}
+        border={true}
+        highlightCurrentRow={true}
+        emptyText='N/A'
+      />
+    )
+  }
+ /* render() {
     return (
       <div id='create'>
         Create 
       </div>
     );
-  }
+  }*/
 }
 
 export default Create;
--- frontend/src/home/spotify-connection.js
+++ frontend/src/home/spotify-connection.js
@@ -3,19 +3,14 @@ import React from 'react';
 // import SpotifyLogo from '../img/spotify-logo.png';
 // <img className='spotify-logo' src={SpotifyLogo} alt="Spotify logo"/>
 
-class Connect extends React.Component {
-    onClickConnect(params) {
-        console.log('Connect to Spotify account');
-    }
 
+class Connect extends React.Component {
     render() {
         return(
             <div className='home-connect'>
                 <div className='home-popup'>
                     <h4> Connect your Spotify account to start now </h4>
-                    <button className='button-connect' onClick={this.onClickConnect.bind(this)}>
-                        Connect to Spotify
-                    </button>
+                    <a className='button-connect' target="_blank" rel="noopener noreferrer" href='/signup'>Connect to Spotify</a>
                 </div>
             </div>
         );
