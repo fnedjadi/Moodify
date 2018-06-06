a8e960caa78ecf1f20788093fbc6e4f6bd15292e
Valentin Dewolf <dewolf_v@epita.fr>
2018-06-06 14:24:04 +0200

    [BACK] updated login endpoint with spotify-web-api-node package

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,22 @@
 const express = require('express')
+const Spotify = require('spotify-web-api-node');
+
 const app = express()
+const CLIENT_ID = '6a37cf1e18fd4b2495b1ebd805e02b9d'; // Your client id
+const CLIENT_SECRET = '1f744596799f4f97a82a9dbf8d1b809c'; // Your secret
+const REDIRECT_URI = 'http://localhost:8080/callback'; // Your redirect uri
+const STATE_KEY = 'spotify_auth_state';
 
-const client_id = '6a37cf1e18fd4b2495b1ebd805e02b9d'; // Your client id
-const client_secret = '1f744596799f4f97a82a9dbf8d1b809c'; // Your secret
-const redirect_uri = 'http://localhost:8080/callback'; // Your redirect uri
-const stateKey = 'spotify_auth_state';
+const AUTH_SCOPES = ['user-read-private', 'user-read-email'];
 
+const spotifyApi = new Spotify({
+  clientId: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  redirectUri: REDIRECT_URI
+});
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Moodify! <a href="/login">login</a>')
 })
 
 app.listen(8080, function () {
@@ -26,10 +34,40 @@ var generateRandomString = function(length) {
   return text;
 };
 
+
+app.get('/login', (_, res) => {
+  const state = generateRandomString(16);
+  res.cookie(STATE_KEY, state);
+  res.redirect(spotifyApi.createAuthorizeURL(AUTH_SCOPES, state));
+});
+
+app.get('/callback', (req, res) => {
+  const { code, state } = req.query;
+  const storedState = req.cookies ? req.cookies[STATE_KEY] : null;
+  // state state validation
+  if (state === null || state !== storedState) {
+    res.redirect('/#/error/state mismatch');
+  } else {
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+      const { expires_in, access_token, refresh_token } = data.body;
+      spotifyApi.setAccessToken(access_token);
+      spotifyApi.setRefreshToken(refresh_token);
+      res.redirect(`/#/user/${access_token}/${refresh_token}`);
+    }).catch(err => {
+      res.redirect('/#/error/invalid token');
+    });
+  }
+});
+
+
+
+
+
 /*
+
 app.use(express.static(__dirname + '/public'))
    .use(cors())
-   .use(cookieParser());*/
+   .use(cookieParser());
 
 app.get('/login', function(req, res) {
 
@@ -47,3 +85,4 @@ app.get('/login', function(req, res) {
       state: state
     }));
 });
+*/
