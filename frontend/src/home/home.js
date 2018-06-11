da85967a005abdf0224b602dff7ac8c0a27367c5
Valentin Dewolf <valentin.dewolf@epita.fr>
2018-06-11 12:02:08 +0200

    [FRONT] removed debug printings in home.js

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -5,42 +5,12 @@ import Subscribe from './get-spotify.js';
 import cookie from 'react-cookies';
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            testData: null
-        }
-    }
-
-    componentDidMount() {
-        /*
-        fetch('http://localhost:8080/moods/playlist?access_token=' + cookie.load('spotify_access_token') + '&mood1=calm')
-            //.then(response => response.json())
-            .then(response => {
-                console.log("responded");
-                console.log(response);
-                console.log("responded");
-
-                this.setState({
-                    testData: response
-                })
-            })
-            */
-    }
-
     render() {
         return (
             <div>
                 <Top />
                 <Connect />
-                <h3>
-                    {document.cookie}
-                </h3>
-                <p>
-                    {this.state.testData ? JSON.stringify(this.state.testData) : "noData"}
-                </p>
                 <Subscribe />
-                
             </div>
         );
     }
